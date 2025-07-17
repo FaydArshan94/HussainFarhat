1b857dffb12535c4a0ba012bb5cf0c15c9ece68d
fayarsh <faydarsh2000@gmail.com>
2025-07-17 10:11:34 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full z-50 bg-transparent text-white px-4 sm:px-10 py-6 flex  justify-between sm:items-center gap-4 sm:gap-0">
+      {/* Logo */}
+      <div className="flex items-center gap-4">
+        <img
+          src="src/assets/abyDRUeZdxkU.png"
+          alt="char"
+          className="h-12 w-12 sm:h-16 sm:w-16 object-cover"
+        />
+      </div>
+
+      <button className="flex items-center gap-2 sm:gap-4">
+        <p className="text-lg hidden sm:initial">Menu</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 48 24"
+          className="h-12 w-12 text-white"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="4"
+        >
+          <g transform="skewX(-40)">
+            <line x1="4" y1="6" x2="40" y2="6" strokeLinecap="square" />
+            <line x1="8" y1="18" x2="36" y2="18" strokeLinecap="square" />
+          </g>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
+const Home = () => {
+  const [showContent, setShowContent] = useState(false);
+  const landingRef = useRef(null);
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.to(".eye", {
+      scale: 10,
+      duration: 2,
+      delay: 1,
+      ease: "expo.inOut",
+      transformOrigin: "50% 50%",
+      opacity: 0,
+      onUpdate: function () {
+        if (this.progress() >= 0.7) {
+          document.querySelector(".svg")?.remove();
+          setShowContent(true);
+          this.kill();
+        }
+      },
+    });
+  });
+
+  useGSAP(() => {
+    if (!showContent) return;
+
+    const landing = landingRef.current;
+
+    gsap.to(landing, {
+      rotate: 0,
+      scale: 1,
+      duration: 1,
+      ease: "expo.inOut",
+    });
+
+    const moveHandler = (e) => {
+      const xmove = (e.clientX / window.innerWidth - 0.5) * 40;
+
+      gsap.to(".photosdiv .text", {
+        x: `${xmove * 0.4}%`,
+      });
+
+      gsap.to(".bg", {
+        x: `${xmove * 1.5}`,
+      });
+    };
+
+    landing?.addEventListener("mousemove", moveHandler);
+
+    return () => landing?.removeEventListener("mousemove", moveHandler);
+  }, [showContent]);
+
+  return (
+    <>
+      {/* SVG Loader */}
+      <div className="svg fixed w-full h-screen  z-[100] flex items-center justify-center overflow-hidden bg-black">
+        <img className="eye h-40" src="src/assets/abyDRUeZdxkU.png" alt="eye" />
+      </div>
+
+      {showContent && (
+        <div className="main w-full overflow-x-hidden">
+          {/* Landing Section */}
+          <div
+            ref={landingRef}
+            className="landing relative   w-full h-screen sm:h-[120vh] overflow-hidden sm:bg-bg-[url(https://maltapersonaltrainer.com/wp-content/uploads/2023/06/hero-bg-1280x725.jpg)] "
+          >
+            <img
+              className="w-full h-full absolute z-0 sepia-50 object-cover"
+              src="https://maltapersonaltrainer.com/wp-content/uploads/2023/06/hero-bg-1280x725.jpg"
+              alt=""
+            />
+
+            <div className="h-[70vh] z-1  w-1/2 sm:w-full sm:h-full flex flex-col sm:flex-row sm:justify-center items-center justify-between sm:items-start absolute top-24 sm:top-[10%] sm:gap-10 left-1/2 -translate-x-1/2">
+              <div className="w-full sm:w-1/3 h-full sm:h-[90vh]  shadow-lg overflow-hidden">
+                <img
+                  className="w-full z-20 h-full object-cover"
+                  src="https://assets.skool.com/f/f48bd8fb890847568a069acef1234924/9d3a70fb9fdd40979bdb6434eec358635b3d696593524c889ec6c819f7ec3aea"
+                  alt=""
+                />
+              </div>
+
+              <div className="z-10 sm:flex sm:flex-col  sm:items-start sm:w-1/3">
+                <div className="sm:flex sm:flex-col  sm:items-start">
+                  <h1 className="text-8xl hidden sm:block font-['Superset'] text-white tracking-wide text-center sm:text-[9rem]">
+                    THE{" "}
+                  </h1>
+                  <h1 className="text-8xl font-['Superset'] text-white tracking-wide text-center sm:text-[9rem]">
+                    GLADIOLUS{" "}
+                  </h1>
+                  <h1 className="text-8xl font-['Superset'] text-white tracking-wide text-center sm:text-[9rem]">
+                    COACHING
+                  </h1>
+                </div>
+
+                <p className="text-white text-center text-md leading-none font-sans-serif mt-4 sm:text-2xl sm:w-full  sm:text-left">
+                  Dream body. No cardio. No diets. Guaranteed results.
+                </p>
+
+                <div className="flex flex-col items-center justify-center mt-8">
+                  <h1 className="text-xl py-[2px] font-sans-serif font-['light'] text-white tracking-wide text-center">
+                    LET'S GO.
+                  </h1>
+                  <div className="w-[5.3rem] h-[3px] bg-zinc-50 ">f</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="shapes w-full h-1/2 absolute z-0 top-1/2  sm:h-full sm:w-full -translate-y-1/2 left-0 flex flex-col items-center justify-center  sm:left-1/2 sm:-translate-x-1/2">
+              <div className="shape h-16 scale-[2] z-0 w-full bg-zinc-950 opacity-100 -rotate-20 sm:w-1/4 sm:h-20 sm:mt-24 sm:-rotate-5 sm:rotate-x-30 sm:-rotate-y-40"></div>
+              <div className="shape h-28 scale-[1.8] mt-10 z-1 w-full bg-zinc-950 opacity-95  rotate-20 sm:rotate-180 sm:h-40 sm:w-[50%] sm:scale-[2] sm:mt-60 sm:rotate-x-30 sm:-rotate-y-40 "></div>
+            </div>
+          </div>
+
+
+          <div className="w-full h-screen bg-black"></div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Home;
