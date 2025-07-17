793f76b1707988d127e80c91c759d96be5ada538
fayarsh <faydarsh2000@gmail.com>
2025-07-17 18:24:37 +0530

    again page5

--- src/components/Page5.jsx
+++ src/components/Page5.jsx
@@ -11,7 +11,7 @@ const Page5 = () => {
           className="bg-video w-full h-full object-cover"
           src="https://maltapersonaltrainer.com/wp-content/uploads/2023/07/join-us.mp4"
         ></video>
-        <div className="video-copy absolute h-full  w-full   select-none bg-black mix-blend-multiply left-0  top-0  flex items-center justify-center  ">
+        <div className="video-copy absolute h-full  w-full  scale-105 sm:scale-100 select-none bg-black mix-blend-multiply left-0  top-0  flex items-center justify-center  ">
           <h1 className="text-[15rem] font-['Superset']  leading-none text-center text-white sm:text-[35rem] ">
             JOIN US
           </h1>
