24cdea5d7312c6d8497374a2c8523db445ea1be4
fayarsh <faydarsh2000@gmail.com>
2025-07-20 22:24:17 +0530

    Page3 Res

--- src/components/Page3.jsx
+++ src/components/Page3.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Page3 = () => {
   return (
-    <div className="w-full relative min-h-screen bg-black">
-      <div className="w-full h-screen ">
+    <div className="w-full relative h-screen px-[1rem] bg-black">
+      <div className="w-full h-screen  ">
         <video
           className="w-full h-full object-cover"
           autoPlay
@@ -14,22 +14,27 @@ const Page3 = () => {
 
         <div className="w-full h-full bg-black/90 absolute left-0 top-0"></div>
 
-        <div className="w-full h-full absolute top-0 left-0 flex flex-col items-start justify-center px-5  sm:flex-row sm:justify-start sm:px-20 sm:items-end sm:gap-60">
-          <h1 className="text-[12rem]  font-['Superset'] uppercase leading-none  text-white  sm:text-[15rem] sm:h-full sm:flex sm:items-center sm:w-1/4">
-            What I do
-          </h1>
+        <div className="w-full px-[2rem]  absolute top-1/2 -translate-y-1/2 left-0 md:top-1/2 md:-translate-y-1/2 flex flex-col md:flex-row md:items-end md:justify-evenly lg:justify-between xl:justify-center md:gap-[3rem] 2xl:gap-[7rem] justify-center    ">
+          <div className="">
+            <h1 className="text-[11rem] w-full 2sm:text-[12rem] md:text-[15rem] xl:text-[17rem] 2xl:text-[18rem] xl:mr-[10rem] font-['Superset'] uppercase leading-none  text-white  ">
+              What
+            </h1>
+            <h1 className="text-[11rem]  2sm:text-[12rem] md:text-[15rem] xl:text-[17rem] 2xl:text-[18rem]  font-['Superset'] uppercase leading-none  text-white  ">
+              WE do
+            </h1>
+          </div>
 
-          <div className="sm:w-1/3 flex flex-col items-start  sm:h-full sm:flex sm:items-start sm:justify-end sm:py-20 sm:flex-col">
-            <p className="text-xl text-white mt-10 leading-[3vh] font-['light'] sm:text-2xl sm:leading-[5vh]">
+          <div className=" flex xs:w-[350px]  lg:w-[40%] 2xl:w-[500px] flex-col items-start  ">
+            <p className="text-xl md:text-2xl xl:text-[1.8rem] 2xl:text-[1.9rem] xl:leading-[4vh] 2xl:leading-[4.5vh] text-white mt-10 leading-[3vh] md:leading-[4vh] font-['light'] ">
               Custom training programs, nutrition coaching, fitness assessments,
               goal setting, progress tracking, motivation, and accountability.
             </p>
 
-            <div className="flex flex-col sm:w-fit items-center justify-center mt-8">
-              <h1 className="text-xl py-[2px] font-sans-serif font-['light'] text-white tracking-wide text-center">
+            <div className="flex flex-col sm:w-fit  items-center justify-center mt-8">
+              <h1 className="text-xl py-[2px] border-b-4 md:text-2xl border-white font-sans-serif font-['light'] text-white tracking-wide text-center">
                 START NOW
               </h1>
-              <div className="w-full h-[3px] bg-zinc-50 "></div>
+              
             </div>
           </div>
         </div>
