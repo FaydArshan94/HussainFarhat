e7b8e75c252cdb1160c8ab2ad7eaa9a348033dd9
fayarsh <faydarsh2000@gmail.com>
2025-07-20 23:32:08 +0530

    Page5 res done

--- src/components/Page5.jsx
+++ src/components/Page5.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 const Page5 = () => {
   return (
     <div className="w-full relative  bg-black py-20 md:py-0 flex items-center justify-center 2xl:py-60 ">
-      <div className="vid-section h-screen  relative overflow-hidden w-[90vw] 2xl:w-full md:h-screen lg:min-h-screen">
+      <div className="vid-section h-screen  relative overflow-hidden w-[90vw] 2xl:w-full  2sm:h-[130vh] mdsm:h-[200vh] md:h-screen 2xl:h-[110vh]">
         <video
           autoPlay
           loop
@@ -11,11 +11,11 @@ const Page5 = () => {
           className="bg-video w-full h-full object-cover"
           src="https://maltapersonaltrainer.com/wp-content/uploads/2023/07/join-us.mp4"
         ></video>
-        <div className="video-copy absolute h-full  w-full  scale-105 2xl:100 select-none bg-black mix-blend-multiply left-0  top-0  flex flex-col md:flex-row md:gap-16 items-center justify-center  ">
-          <h1 className="text-[15rem] lg:text-[33vw] 2xl:text-[38vw] font-['Superset'] tracking-wide leading-none text-center text-white  ">
+        <div className="video-copy absolute h-full  w-full  scale-105 2sm:scale-100 2xl:scale-100 select-none bg-black mix-blend-multiply left-0  top-0  flex flex-col md:flex-row md:gap-16 items-center justify-center  ">
+          <h1 className="text-[15rem] 2sm:text-[30rem] mdsm:text-[45rem] md:text-[25rem] lg:text-[28vw] 2xl:text-[40vw] font-['Superset'] tracking-wide leading-none text-center text-white  ">
             JOIN
           </h1>
-          <h1 className="text-[15rem] lg:text-[33vw] 2xl:text-[38vw] font-['Superset'] tracking-wide leading-none text-center text-white  ">
+          <h1 className="text-[15rem] 2sm:text-[30rem] mdsm:text-[45rem] md:text-[25rem] lg:text-[28vw] 2xl:text-[40vw] font-['Superset'] tracking-wide leading-none text-center text-white  ">
              US
           </h1>
         </div>
