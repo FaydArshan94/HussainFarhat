bb5843e543930eb2e3a3a88efbd0ed3fada7614c
fayarsh <faydarsh2000@gmail.com>
2025-07-21 10:26:16 +0530

    Page 7 and Footer Res

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,27 @@
+import { FaLocationDot } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
+import logo from "../assets/logo.png";
 
-import logo from '../assets/logo.png'
 const Footer = () => {
   return (
-    <footer className="bg-black h-screen text-white px-6 py-10 flex flex-col items-center justify-evenly text-center sm:h-[200vh]">
+    <footer className="bg-black min-h-screen text-white px-6 py-10 xs:px-20 flex flex-col items-center justify-evenly text-center 2xl:px-32 2xl:py-20">
       {/* Logo */}
       <div className="flex flex-col items-center space-y-2">
-        <img
-          className="w-20 h-20 sm:h-32 sm:w-32"
-          src={logo}
-          alt=""
-        />
-        <div className="text-base tracking-widest text-gray-300 sm:text-2xl">
+        <img className="w-20 h-20 sm:h-32 sm:w-32 2xl:w-40 2xl:h-40" src={logo} alt="logo" />
+        <div className="text-base tracking-widest text-gray-300 sm:text-2xl 2xl:text-3xl">
           THE GLADIOLUS COACHING PROGRAM
         </div>
       </div>
 
       {/* Tagline */}
-      <div className="text-5xl leading-tight font-['Superset']   sm:text-8xl">
+      <div className="text-5xl mt-10 leading-tight font-['Superset'] sm:text-8xl md:mt-40 2xl:text-[10rem]">
         YOUR FITNESS <br />
         <span className="text-gray-400">JOURNEY</span>{" "}
         <span className="text-[#890900]">IS HERE</span>
       </div>
 
       {/* Navigation Links */}
-      <div className="flex flex-wrap justify-center gap-6 text-lg text-gray-300 sm:mt-20 sm:text-xl sm:gap-10">
+      <div className="flex flex-wrap justify-center gap-6 text-lg text-gray-300 mt-10 2sm:mt-28 sm:text-xl sm:gap-10 2xl:text-2xl 2xl:gap-14">
         <a href="#">Home</a>
         <a href="#">About</a>
         <a href="#">Cookies Policy</a>
@@ -33,12 +31,16 @@ const Footer = () => {
       </div>
 
       {/* Address and Contact */}
-      <div className="text-sm text-gray-400  flex flex-col sm:mt-20 sm:flex items-center justify-center   sm:space-y-0 sm:gap-0 ">
-        <div className="flex items-center justify-center gap-2 sm:text-lg ">
-          <span>📍</span>
+      <div className="text-md text-gray-400 mt-10 flex flex-col sm:mt-20 sm:flex items-center justify-center sm:space-y-0 sm:gap-0 2xl:text-xl 2xl:gap-4">
+        <div className="flex items-center w-fit justify-around gap-2 sm:text-lg 2xl:text-2xl">
+          <div className="text-xl 2xl:text-3xl">
+            <FaLocationDot />
+          </div>
           <span>Virtaga HQ 2nd floor BKR9022 Bridgeview, Illinois</span>
         </div>
-        <div className="h-8 w-8 mt-2 flex items-center justify-center">
+
+        {/* Middle line */}
+        <div className="h-8 w-8 mt-2 flex items-center justify-center 2xl:h-10 2xl:w-10">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 48 24"
@@ -53,16 +55,20 @@ const Footer = () => {
           </svg>
         </div>
 
-        <div className="flex items-center justify-center gap-2 sm:text-lg  sm:items-start">
+        <div className="flex items-center justify-center gap-2 sm:text-lg sm:items-start 2xl:text-2xl">
+          <div className="text-xl 2xl:text-3xl">
+            <IoMdMail />
+          </div>
           <span>instagram.com/hussein.fht</span>
         </div>
       </div>
 
       {/* Bottom */}
-      <div className="text-sm italic mt-10 flex flex-col items-center text-gray-500 font-['light'] space-y-1 sm:flex-row sm:gap-10 sm:mt-16 sm:text-center sm:not-italic">
+      <div className="text-sm italic mt-10 flex flex-col items-center text-gray-500 font-['light'] space-y-1 sm:flex-row sm:gap-10 sm:mt-16 sm:text-center sm:not-italic 2xl:text-lg 2xl:gap-16">
         <div>Hussein Farhat</div>
         <div>Copyright 2025</div>
-        <div className="h-5 sm:hidden w-5 mt-2 flex items-center justify-center">
+
+        <div className="h-5 sm:hidden w-5 mt-2 flex items-center justify-center 2xl:h-6 2xl:w-6">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 48 24"
@@ -76,8 +82,10 @@ const Footer = () => {
             </g>
           </svg>
         </div>
+
         <div>All rights reserved</div>
-        <div className="h-5 hidden  w-5 mt-2 sm:flex items-center justify-center">
+
+        <div className="h-5 hidden w-5 mt-2 sm:flex items-center justify-center 2xl:h-6 2xl:w-6">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 48 24"
@@ -91,6 +99,7 @@ const Footer = () => {
             </g>
           </svg>
         </div>
+
         <div>Created By Fayd Arshan</div>
       </div>
     </footer>
--- src/components/Page7.jsx
+++ src/components/Page7.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Page7 = () => {
   return (
-    <div className="relative w-full min-h-[55vh] flex items-center justify-center bg-white/30  text-white px-4 py-10 overflow-hidden sm:h-[70vh]">
+    <div className="relative w-full h-[70vh] flex items-center justify-center bg-white/30  text-white px-4 py-10 overflow-hidden sm:h-[70vh]">
       {/* Static Marquee Background */}
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2  -translate-y-1/2 w-full h-full z-0 flex flex-col gap-4 opacity-[0.05] pointer-events-none">
-        {Array.from({ length: 3 }).map((_, i) => (
+        {Array.from({ length: 5 }).map((_, i) => (
           <h1
             key={i}
-            className="whitespace-nowrap text-[10rem] sm:leading-[25vh] font-['Superset'] text-white  tracking-normal sm:text-[12rem]"
+            className="whitespace-nowrap text-[10rem] leading-none font-['Superset'] text-white  tracking-normal sm:text-[12rem]"
           >
             DON'T PUT IT OFFDON'T PUT IT OFFDON'T PUT IT OFFDON'T PUT IT OFF
           </h1>
@@ -17,7 +17,7 @@ const Page7 = () => {
 
       {/* Main Foreground Content */}
       <div className=" z-10 max-w-2xl text-center w-full  absolute left-1/2 top-1/2 -translate-x-1/2  -translate-y-1/2 sm:max-w-4xl">
-        <h1 className="text-[10rem] font-['Superset'] leading-none sm:text-[12rem] sm:w-full">
+        <h1 className="text-[9rem] font-['Superset'] leading-none sm:text-[12rem] sm:w-full">
           DON'T PUT  IT OFF
         </h1>
         <h1 className="mt-6 w-fit mx-auto text-lg font-['light'] sm:text-xl tracking-wider font-light border-b-4 border-white hover:bg-white hover:text-[#003EFF] transition duration-300 cursor-pointer">
